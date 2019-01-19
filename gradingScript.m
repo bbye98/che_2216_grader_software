f81427276e76666c5e36628dca8a81e11c618437
Benjamin Ye <benjaminbye98@gmail.com>
2019-01-19 02:24:35 -0500

    Fixed 'user_pref.mat' cell initiation issue where values couldn't be stored because parentheses were used in place of curly brackets

--- gradingScript.m
+++ gradingScript.m
@@ -1,8 +1,8 @@
 if exist('user_pref.mat', 'file') ~= 2
     user_pref = cell(2, 2);
     user_pref(:, 1) = {'opt_show_instructions_roster'; 'opt_show_instructions_submissions'};
-    user_pref{:, 2} = [1; 1];
-    save('user_pref.mat', user_pref)
+    user_pref(:, 2) = {1; 1};
+    save('user_pref.mat', 'user_pref')
 end
 load('user_pref.mat')
 newSemesterCheck(user_pref)
