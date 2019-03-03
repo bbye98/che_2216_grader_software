7cb1513e1353c19b57b10cb5c70d6e1a7c4587b9
bbye98 <benjaminbobinye@gmail.com>
2019-03-03 03:41:01 -0500

    Added pop-up message box when there are no pending grades to be uploaded.

--- uploadGrades.m
+++ uploadGrades.m
@@ -5,7 +5,6 @@ function [] = uploadGrades()
 
 q_uploadGrades = questdlg('Would you like to upload grades to UVACollab?', '', 'Yes', 'No', 'No');
 if strcmp(q_uploadGrades, 'Yes')
-    word = actxserver('Word.Application');
     origDir = pwd;
     assignmentsDir = struct2cell(dir('Assignments'))';
     studentsDir = struct2cell(dir('Students'))';
@@ -22,32 +21,36 @@ if strcmp(q_uploadGrades, 'Yes')
             error(['You cannot upload grades for ' assignmentName ' at this time.'])
         end
     end
-    uploadedInd = find(~ contains(gradeStatus(:, 3), 'Not Uploaded'));
-    uploadedStudents = gradeStatus(uploadedInd, 1);
-    if ~ isempty(uploadedStudents)
-        q_addUploadedStudents = questdlg('Would you like to reupload grades for any students?', '', 'Yes', 'No', 'No');
-        if strcmp(q_addUploadedStudents, 'Yes')
-            q_addUploadedStudentsSel = listdlg('PromptString', 'Choose student(s) to add.', 'ListString', uploadedStudents);
-            gradeStatus(uploadedInd(q_addUploadedStudentsSel), 2) = {'Not Graded'};
-            save(['Assignments\' assignmentName '\gradeStatus.mat'], 'gradeStatus')
+    if isempty(find(contains(gradeStatus(contains(gradeStatus(:, 3), 'Not Uploaded'), 2), 'Not Graded'), 1))
+        word = actxserver('Word.Application');
+        uploadedInd = find(~ contains(gradeStatus(:, 3), 'Not Uploaded'));
+        uploadedStudents = gradeStatus(uploadedInd, 1);
+        if ~ isempty(uploadedStudents)
+            q_addUploadedStudents = questdlg('Would you like to reupload grades for any students?', '', 'Yes', 'No', 'No');
+            if strcmp(q_addUploadedStudents, 'Yes')
+                q_addUploadedStudentsSel = listdlg('PromptString', 'Choose student(s) to add.', 'ListString', uploadedStudents);
+                gradeStatus(uploadedInd(q_addUploadedStudentsSel), 2) = {'Not Graded'};
+                save(['Assignments\' assignmentName '\gradeStatus.mat'], 'gradeStatus')
+            end
         end
-    end
-    remainingStudentsInd = find(contains(gradeStatus(:, 3), 'Not Uploaded'));
-    remainingStudentsInd = remainingStudentsInd(~ contains(gradeStatus(remainingStudentsInd, 2), 'Not Graded'));
-    for studentLoop = 1:length(remainingStudentsInd)
-        cd(['Students\' studentsDir{remainingStudentsInd(studentLoop)} '\' assignmentName])
-        wdoc = word.Documents.Open([pwd '\graderComments.doc']);
-        clipboard('copy', wdoc.Content.Text);
-        wdoc.Close
-        cd(origDir)
-        q_uploadCheck = questdlg([studentsDir{remainingStudentsInd(studentLoop)} '''s scores have been saved to the clipboard. Press Done after entering grades on UVACollab or Cancel to abort.'], '', 'Done', 'Cancel', 'Cancel');
-        if strcmp(q_uploadCheck, 'Done')
-            gradeStatus{remainingStudentsInd(studentLoop), 3} = 'Uploaded';
-            save([origDir '\Assignments\' assignmentName '\gradeStatus.mat'], 'gradeStatus')
-        else
-            error('You chose to abort the program.')
+        remainingStudentsInd = find(contains(gradeStatus(:, 3), 'Not Uploaded'));
+        remainingStudentsInd = remainingStudentsInd(~ contains(gradeStatus(remainingStudentsInd, 2), 'Not Graded'));
+        for studentLoop = 1:length(remainingStudentsInd)
+            cd(['Students\' studentsDir{remainingStudentsInd(studentLoop)} '\' assignmentName])
+            wdoc = word.Documents.Open([pwd '\graderComments.doc']);
+            clipboard('copy', wdoc.Content.Text);
+            wdoc.Close
+            cd(origDir)
+            q_uploadCheck = questdlg([studentsDir{remainingStudentsInd(studentLoop)} '''s scores have been saved to the clipboard. Press Done after entering grades on UVACollab or Cancel to abort.'], '', 'Done', 'Cancel', 'Cancel');
+            if strcmp(q_uploadCheck, 'Done')
+                gradeStatus{remainingStudentsInd(studentLoop), 3} = 'Uploaded';
+                save([origDir '\Assignments\' assignmentName '\gradeStatus.mat'], 'gradeStatus')
+            else
+                error('You chose to abort the program.')
+            end
         end
+        word.Quit
+    else
+        waitfor(msgbox(['There are no pending grades to be uploaded for ' assignmentName '.']))
     end
-    word.Quit
-end
 end
